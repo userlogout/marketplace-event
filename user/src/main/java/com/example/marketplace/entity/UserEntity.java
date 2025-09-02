bc89925ba21798ad9487288dbb7a025e79b331b3
userlogout <randomlevel@yandex.ru>
2025-09-02 21:57:40 +0300

    user-jpa-draft

---
+++ user/src/main/java/com/example/marketplace/entity/UserEntity.java
@@ -0,0 +1,7 @@
+package com.example.marketplace.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class UserEntity {
+}
---
+++ user/src/main/java/com/example/marketplace/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.marketplace.repository;
+
+@Repository
+public interface UserRepository {
+
+}
